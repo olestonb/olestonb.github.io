481d789d90bbb1862326f25698f7453e2eb4a0c2
Brett Oleston <oleston.b@stu.janesville.k12.wi.us>
2023-04-17 13:44:08 -0500

    Update

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -8,4 +8,12 @@ function calc() {
     var r = Number(document.getElementById("interest").value);
     var t = Number(document.getElementById("years").value);
     var m = Number(document.getElementById("payment").value);
+    var n = 12;
+    var B = 0;
+    var out = document.getElementById("out");
+    out.innerText= "";
+
+    B = (P * (1 + r / n)) ** (n * t) - (m * ((1 + r / n) ** (n * t) - 1) / (r / n));
+
+    out.innerText += "Remaining loan balance: $" + B.toFixed(2);
 }
